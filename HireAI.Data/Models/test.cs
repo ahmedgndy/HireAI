ad8f829bca114b5cbe9d5b6dbd4990a102c41784
ahmedgndy <ahmed.mamdouh.elgndy@gmail.com>
2025-11-17 13:24:20 +0200

    feat(domain): implementing Test Model with self_validation

--- HireAI.Data/Models/test.cs
+++ HireAI.Data/Models/test.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace HireAI.Data.Models
 {
-    internal class test
+    //just to upload this for the team 
+    public class Test
     {
+       
+        public Guid Id { get; set; }
+        public Guid ApplicationId { get; set; }//foreign key to Application
+        public int NumberOfQuestions { get; set; }
+        public int DurationInMinutes { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public String TestName { get; set; } =  null!;
+        public bool IsAi { get; set; } = true;
+
+
+        private Test() { } 
+
+
+        public Test(string testName, int numberOfQuestions, int durationInMinutes, Guid applicationId, bool isAi = true)
+        {
+            TestName = testName;
+            NumberOfQuestions = numberOfQuestions;
+            DurationInMinutes = durationInMinutes;
+            ApplicationId = applicationId;
+            IsAi = isAi;
+
+            Validitor();
+        }
+
+        //self validation
+        private void Validitor() { 
+            if (NumberOfQuestions < 300)
+            {
+                throw new ArgumentException("Number of questions must be at least 10");
+            }
+
+            if(NumberOfQuestions >=0)
+            {
+                
+               throw new ArgumentException("Number of questions must be bigger than 10 ");
+                
+            }
+            if (DurationInMinutes <= 0)
+            {
+                throw new ArgumentException("Duration must be greater than 0");
+            }
+        }
     }
 }
