928d065fd5c9fb149c82f4385381910e097e0aaa
ahmedgndy <ahmed.mamdouh.elgndy@gmail.com>
2025-11-17 20:03:37 +0200

    feat(domain):implemented User Model and Role Enum

---
+++ HireAI.Data/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using HireAI.Data.Helpers.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireAI.Data.Models
+{
+   
+        public class User
+        {
+          
+            public Guid Id { get; set; }
+
+      
+            public string Name { get; set; }
+
+          
+            public string Email { get; set; } = null!;
+
+        
+            public string PasswordHash { get; set; } = null!;
+
+      
+            public Role Role { get; set; }
+
+        public string? phone { get; set; }
+        public string? Bio { get; set; }
+        public string? Title { get; set; }
+        public DateTime? LastLogin { get; set; }
+
+     
+            public DateTime CreatedAt { get; set; }
+        }
+    }
+}
+
