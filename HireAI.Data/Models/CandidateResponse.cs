e828aaa6f137ebcd97c85ec1236ad04108ee7415
Hamza Al-Gohary <hamzaelgohary15_18@yahoo.com>
2025-11-17 18:56:51 +0200

    feat(domain): added CandidateResponse model

--- HireAI.Data/Models/CandidateResponse.cs
+++ HireAI.Data/Models/CandidateResponse.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace HireAI.Data.Models
 {
-    internal class CandidateResponse
+    public class CandidateResponse
     {
+        public Guid Id { get; set; }
+        public Guid TestId { get; set; }
+        public Guid QuestionId { get; set; }
+        public Guid AnswerId { get; set; }
     }
 }
