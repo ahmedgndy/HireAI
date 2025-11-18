e576e620967ef123b0b778892609e8a0931ccf87
ahmedgndy <ahmed.mamdouh.elgndy@gmail.com>
2025-11-18 22:48:02 +0200

    feat(infrastructure): add appdbcontext and project structure and update Entity Framework

---
+++ HireAI.Infrastructure/Context/HireAIDbContext.cs
@@ -0,0 +1,54 @@
+using HireAI.Data.Models;
+using HireAI.Data.Helpers.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace HireAI.Infrastructure.Context
+{
+    public class HireAIDbContext : DbContext
+    {
+        public HireAIDbContext(DbContextOptions<HireAIDbContext> options) : base(options)
+        {
+        }
+
+        // DbSets for concrete entities
+        public DbSet<User> Users { get; set; } = default!;
+        public DbSet<Applicant> Applicants { get; set; } = default!;
+        public DbSet<HR> HRs { get; set; } = default!;
+        public DbSet<JobOpening> JobOpenings { get; set; } = default!;
+        public DbSet<Application> Applications { get; set; } = default!;
+        public DbSet<Payment> Payments { get; set; } = default!;
+        public DbSet<CV> CVs { get; set; } = default!;
+
+        public DbSet<Answer> Answers { get; set; } = default!;
+        public DbSet<ApplicantSkill> ApplicantSkills { get; set; } = default!;
+        public DbSet<ApplicantResponse> ApplicantResponses { get; set; } = default!;
+        public DbSet<Exam> Exams { get; set; } = default!;
+        public DbSet<ExamEvaluation> ExamEvaluation { get; set; } = default!;
+        public DbSet<ExamSummary> ExamSummarys { get; set; } = default!;
+        public DbSet<JobSkill> JobSkills { get; set; } = default!;
+        public DbSet<QuestionEvaluation> QuestionEvaluations { get; set; } = default!;
+        public DbSet<Question> Questions { get; set; } = default!;
+        public DbSet<Skill> Siklls { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //TPC
+            modelBuilder.Entity<HR>().ToTable("HRs");
+            modelBuilder.Entity<Applicant>().ToTable("Applicant");
+
+            // Apply configuration classes from this assembly (your IEntityTypeConfiguration implementations)
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(HireAIDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=HireAIDb;Trusted_Connection=True;TrustServerCertificate=True;\"\r\n");
+
+        }
+    }
+}
