1c80d234f9ed8c7d1b46b0150cd0856c857d5982
ahmedgndy <ahmed.mamdouh.elgndy@gmail.com>
2025-11-26 01:31:56 +0200

    feat(DTO) : Add DTO classes for exam, question, and answer models

---
+++ HireAI.Data/Helpers/DTOs/ExamDTOS/Respones/ExamDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using HireAI.Data.Helpers.Enums;
+
+namespace HireAI.Data.Helpers.DTOs.ExamDTOS.Respones
+{
+    public class ExamDTO
+    {
+        public int Id { get; set; }
+        public int NumberOfQuestions { get; set; }
+        public int DurationInMinutes { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ExamName { get; set; } = default!;
+        public bool IsAi { get; set; } = true;
+        public enExamType ExamType { get; set; } = enExamType.MockExam;
+
+        public int? ApplicantId { get; set; }
+        public int? ApplicationId { get; set; }
+
+        public List<QuestionDTO> Questions { get; set; } = new List<QuestionDTO>();
+    }
+
+  
+
+}
---
+++ HireAI.Data/Helpers/DTOs/ExamDTOS/Respones/QuestionDTO.cs
@@ -0,0 +1,23 @@
+﻿using HireAI.Data.Helpers.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireAI.Data.Helpers.DTOs.ExamDTOS.Respones
+{
+    public class QuestionDTO
+    {
+        public int Id { get; set; }
+        public string QuestionText { get; set; } = default!;
+        public enQuestionAnswers? Answer { get; set; }
+        public int QuestionNumber { get; set; }
+
+        public int ExamId { get; set; }
+        public int? ApplicantResponseId { get; set; }
+
+        public List<AnswerDTO> Answers { get; set; } = new List<AnswerDTO>();
+    }
+  
+}
