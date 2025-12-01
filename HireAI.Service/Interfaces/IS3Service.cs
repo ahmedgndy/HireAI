d1afccbfc23fe63d6f06acdc6df28c15223375de
ahmedgndy <ahmed.mamdouh.elgndy@gmail.com>
2025-12-01 07:39:39 +0200

    feat(s3servied) :  Add S3 file upload functionality and dependencies

---
+++ HireAI.Service/Interfaces/IS3Service.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireAI.Service.Interfaces
+{
+    public interface IS3Service
+    {
+        public Task<String> UploadFileAsync(IFormFile file);
+    }
+}
---
+++ HireAI.Service/Services/S3Service.cs
@@ -0,0 +1,48 @@
+﻿using Amazon.S3;
+using Amazon.S3.Transfer;
+using HireAI.Service.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireAI.Service.Services
+{
+    public class S3Service : IS3Service
+    {
+        private readonly IAmazonS3 _s3;
+        private readonly string _bucketName = "hireai-bucket"; 
+        private  readonly string _region = "us-east-1";
+        public S3Service(IAmazonS3 s3) {
+            _s3 = s3;
+        }
+        public async Task<string> UploadFileAsync(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                throw new ArgumentException("File is null or empty", nameof(file));
+            }
+
+            var  key = $"cv/{Guid.NewGuid()}_{file.FileName}";
+
+           using var stream = file.OpenReadStream();    
+           
+           var transferUtility = new TransferUtility(_s3);
+
+           var transferRequest = new TransferUtilityUploadRequest
+           {
+               InputStream = stream,
+               Key = key,
+               BucketName = _bucketName,
+               ContentType = file.ContentType
+           };
+
+          await transferUtility.UploadAsync(transferRequest);
+
+          var url = $"https://{_bucketName}.s3.{_region}.amazonaws.com/{key}";
+          return url;
+        }
+    }
+}
